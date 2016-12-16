77768999857c5277d847f0291046caa16fc3d44b
jaguilar <julie.aguilar@cgi.com>
2016-12-15 20:33:49 -0800

    Updated error messages and UI. Removed remember me field.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username , presence: { :message => "The username is required"} ,uniqueness: { :message => "The username is taken"}
-  validates :password, presence: { :message => "The password is required"}
-  validates :password_confirmation, presence: { :message => "The confirm password is required"}
-  validates :zipcode, presence: { :message => "The zipcode is required"},
-   numericality: { only_integer: true ,:message => "The ZIP must be numeric"},
-   length: { is: 5 , :message => "ZIP Code should be 5 characters"}
-  validates :email, presence: { :message => "The email is required"}
-  validates :lastname, presence: { :message => "The lastname is required"}, length: { maximum: 40 }
+  validates :username , presence: { :message => "The Username is required"} ,uniqueness: { :message => "The Username is taken"}
+  validates :password, presence: { :message => "The Password is required"}
+  validates :password_confirmation, presence: { :message => "The Confirm Password is required"}
+  validates :zipcode, presence: { :message => "The Zip Code is required"},
+   numericality: { only_integer: true ,:message => "The ZIP Code must be numeric"},
+   length: { is: 5 , :message => "The ZIP Code should be 5 characters"}
+  validates :email, presence: { :message => "The Email is required"}
+  validates :lastname, presence: { :message => "The Last Name is required"}, length: { maximum: 40 }
 end
--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,108 +1,94 @@
 require 'spec_helper'
 
 
-# feature 'User sees landing page' do
-#   scenario 'with splash image, empty input fields, and buttons' do
-#     visit "/"
+feature 'User sees landing page' do
+  scenario 'with splash image, empty input fields, and buttons' do
+    visit "/"
 
-#     #Login
-#     expect(page).to have_content('Username')
-#     expect(page).to have_content('Password')
-#     expect(page).to have_content('Remember me')
+    #Login
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Password')
     
-#     #NewUser
-#     expect(page).to have_content('Username')
-#     expect(page).to have_content('Password')
-#     expect(page).to have_content('Confirm password')
-#     expect(page).to have_content('Last Name')
-#     expect(page).to have_content('Email')
-#     expect(page).to have_content('ZIP')
-#     click_button "Create Account"
-#     expect(page).to have_content("User Name can't be blank")
-#   end
-# end
+    #NewUser
+    expect(page).to have_content('Username')
+    expect(page).to have_content('Password')
+    expect(page).to have_content('Confirm Password')
+    expect(page).to have_content('Last Name')
+    expect(page).to have_content('Email')
+    expect(page).to have_content('ZIP Code')
+    #expect(page).to have_image('capture.png')
+    click_button "Create Account"
+    expect(page).to have_content("Username is required")
+  end
+end
 
-# feature 'User enters required fields' do
-#   scenario 'with valid values' do
-#     visit "/"
+feature 'User enters required fields' do
+  scenario 'with valid values' do
+    visit "/"
 
-# # #Login
-# def sign_up_with(username, password)
-#     visit "/"
-#     fill_in 'Username', with: jaguilar
-#     fill_in 'Password', with: password
-#     fill_in 'Remember me', with: password
-#     fill_in 'Password', with: password
-#     click_button "Login"
-#   end
+#Login
+def sign_up_with(username, password)
+    visit "/"
+    fill_in 'Username', with: username
+    fill_in 'Password', with: password
+    click_button "Login"
+  end
 
-# #Register
-# def sign_up_with(email, password)
-#     visit "/"
-#     fill_in 'Username', with: email
-#     fill_in 'Password', with: password
-#     fill_in 'Confirm password', with: password
-#     fill_in 'Last Name', with: password
-#     fill_in 'Email', with: password
-#     fill_in 'ZIP', with: password
-#     click_button "Login"
-#   end
+#Register
+def sign_up_with(username, password)
+    visit "/"
+    fill_in 'Username', with: username
+    fill_in 'Password', with: password
+    fill_in 'Confirm password', with: confirmpassword
+    fill_in 'Last Name', with: lastname
+    fill_in 'Email', with: email
+    fill_in 'ZIP', with: zip
+    click_button "Create Account"
+  end
 
-#   end
-# end
+  end
+end
 
-# spec/features/visitor_signs_up_spec.rb
+# # spec/features/visitor_signs_up_spec.rb
 # require 'spec_helper'
 
+feature 'user create account' do
+  scenario 'with valid lastname, ' do
+    sign_up_with 'jaguilar'
+
+    #expect(page).to have_content('Log In')
+  end
+
+  scenario 'with invalid lastname' do
+    sign_up_with 'jasguilar'
+
+    #expect(page).to have_content('Log In')
+  end
+
+  def sign_up_with(lastname)
+    visit "/"
+    fill_in 'user_lastname', with: lastname
+    click_button "Create Account"
+  end
+end
+
 # feature 'user create account' do
-#   scenario 'with valid username, password, ' do
+#   scenario 'with valid username' do
 #     sign_up_with 'jaguilar'
 
 #     expect(page).to have_content('Sign In')
 #   end
 
-#   scenario 'with invalid lastname' do
+#   scenario 'with invalid username' do
 #     sign_up_with 'jasguilar'
 
 #     expect(page).to have_content('Sign In')
 #   end
 
-#   # scenario 'with blank password' do
-#   #   sign_up_with 'valid@example.com', ''
-
-#   #   expect(page).to have_content('Sign in')
-#   # end
-
-#   def sign_up_with(lastname)
+#   def sign_up_with(username)
 #     visit "/"
-#     fill_in 'user_lastname', with: lastname
+#     within('form#new_user') do
+#     fill_in 'user_username', with: username
 #     click_button "Create Account"
 #   end
 # end
-
-feature 'user create account' do
-  scenario 'with valid username' do
-    sign_up_with 'jaguilar'
-
-    expect(page).to have_content('Sign In')
-  end
-
-  scenario 'with invalid username' do
-    sign_up_with 'jasguilar'
-
-    expect(page).to have_content('Sign In')
-  end
-
-  # scenario 'with blank password' do
-  #   sign_up_with 'valid@example.com', ''
-
-  #   expect(page).to have_content('Sign in')
-  # end
-
-  def sign_up_with(username)
-    visit "/"
-    within('form#new_user') do
-    fill_in 'user_username', with: username
-    click_button "Create Account"
-  end
-end
