dc776ecd4e9ca4f43619044534ef28c8b78f0a5b
ravicgi <ravikanth.akkanapally.com>
2016-12-19 13:52:38 -0800

    Implemented method for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username , presence: { :message => "The Username is required"} ,uniqueness: { :message => "The Username is taken"}
-  validates :password, presence: { :message => "The Password is required"}
-  validates :password_confirmation, presence: { :message => "The Confirm Password is required"}
+  validates :username , presence: { :message => "The Username is required"} ,uniqueness: { :message => "The Username is taken"} 
+  validates :password , presence: { :message => "The  Password is required"}
+  validates :password_confirmation, presence: { :message => "The Confirm Password is required"} 
   validates :zipcode, presence: { :message => "The Zip Code is required"},
    numericality: { only_integer: true ,:message => "The ZIP Code must be numeric"},
    length: { is: 5 , :message => "The ZIP Code should be 5 characters"}
   validates :email, presence: { :message => "The Email is required"}
   validates :lastname, presence: { :message => "The Last Name is required"}, length: { maximum: 40 }
+  def email_required?
+  false
+end
+
+def email_changed?
+  false
+end
 end
