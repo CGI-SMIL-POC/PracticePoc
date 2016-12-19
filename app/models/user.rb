a2200267e9dfcaa9a4bfd40682f3cc1689997fbe
ravicgi <ravikanth.akkanapally.com>
2016-12-19 14:22:29 -0800

    Added length for feilds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username , presence: { :message => "The Username is required"} ,uniqueness: { :message => "The Username is taken"} 
-  validates :password , presence: { :message => "The  Password is required"}
-  validates :password_confirmation, presence: { :message => "The Confirm Password is required"} 
+  validates :username, presence: { :message => "The Username is required"}, uniqueness: { :message => "The Username is taken"}, length: { maximum: 50}
+  validates :password, presence: { :message => "The  Password is required"}, length: { maximum: 20}
+  validates :password_confirmation, presence: { :message => "The Confirm Password is required"}, length: { maximum: 20}
   validates :zipcode, presence: { :message => "The Zip Code is required"},
    numericality: { only_integer: true ,:message => "The ZIP Code must be numeric"},
-   length: { is: 5 , :message => "The ZIP Code should be 5 characters"}
-  validates :email, presence: { :message => "The Email is required"}
+   length: { is: 5, :message => "The ZIP Code should be 5 characters"}
+  validates :email, presence: { :message => "The Email is required"}, length: { maximum: 100}
   validates :lastname, presence: { :message => "The Last Name is required"}, length: { maximum: 40 }
   def email_required?
   false
