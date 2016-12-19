054fcb4018b0801cc8fb01a7e6b3692d30d8ef04
ravicgi <ravikanth.akkanapally.com>
2016-12-19 14:05:02 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209063759) do
+ActiveRecord::Schema.define(version: 20161219181547) do
+
+  create_table "caseworker", force: :cascade do |t|
+    t.integer  "CaseWorkerId", limit: 4
+    t.string   "FirstName",    limit: 40
+    t.string   "LastName",     limit: 40, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "child", force: :cascade do |t|
+    t.integer  "ChildId",         limit: 4
+    t.string   "FirstName",       limit: 40
+    t.string   "MiddleInitial",   limit: 1
+    t.string   "LastName",        limit: 40, null: false
+    t.string   "Phone",           limit: 10
+    t.string   "Email",           limit: 40, null: false
+    t.date     "DateOfBirth"
+    t.boolean  "DeleteIndicator",            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parent", force: :cascade do |t|
+    t.integer  "ParentId",      limit: 4
+    t.string   "UserName",      limit: 50,  null: false
+    t.string   "Password",      limit: 20,  null: false
+    t.string   "FirstName",     limit: 20
+    t.string   "MiddleInitial", limit: 1
+    t.string   "LastName",      limit: 40,  null: false
+    t.string   "Suffix",        limit: 5
+    t.date     "DateOfBirth"
+    t.string   "Address",       limit: 100
+    t.integer  "Zip",           limit: 8
+    t.string   "HomePhone",     limit: 10
+    t.string   "CellPhone",     limit: 10
+    t.string   "Email",         limit: 100, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parentchild", force: :cascade do |t|
+    t.integer  "ParentChildId",   limit: 4
+    t.integer  "ParentId",        limit: 4, null: false
+    t.integer  "ChildId",         limit: 4, null: false
+    t.integer  "CaseWorkerId",    limit: 4, null: false
+    t.boolean  "DeleteIndicator",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -36,9 +85,10 @@ ActiveRecord::Schema.define(version: 20161209063759) do
     t.string   "state",                  limit: 255
     t.string   "zipcode",                limit: 255
     t.string   "phone",                  limit: 255
+    t.string   "suffix",                 limit: 255
+    t.string   "marital_status",         limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
