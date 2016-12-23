0658016e759adb1a87ccd3d74d311c8060a68556
jaguilar <julie.aguilar@cgi.com>
2016-12-23 10:56:52 -0800

    Fixed validation for password.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,15 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:firstname,:middlename,:lastname,:dob,:street,:city,:state,:zipcode,:phone,:username, :email, :password, :password_confirmation)
   end
 
+
   def account_update_params
     params.require(:user).permit(:firstname,:middlename,:lastname,:dob,:street,:city,:state,:zipcode,:phone,:username, :email, :password, :password_confirmation, :current_password)
   end
+
+  protected
+
+  def update_resource(resource, params)
+  	params.delete :current_password
+    resource.update_without_password(params)
+end
 end
