cd06da76891b8382f40370a03ec25d6adcae3453
jaguilar <julie.aguilar@cgi.com>
2016-12-23 10:22:05 -0800

    Updated view and edit parent profile.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: { :message => "The Username is required"}, uniqueness: { :message => "The Username is taken"}, length: { maximum: 50}
-  validates :password, presence: { :message => "The  Password is required"}, length: { maximum: 20}
-  validates :password_confirmation, presence: { :message => "The Confirm Password is required"}, length: { maximum: 20}
+  validates :password, presence: { :message => "The  Password is required"}, length: { maximum: 20}, :confirmation => true, :on => :create
+  validates :password_confirmation, presence: { :message => "The Confirm Password is required"}, length: { maximum: 20}, :confirmation => true, :on => :create
   validates :zipcode, presence: { :message => "The Zip Code is required"},
    numericality: { only_integer: true ,:message => "The ZIP Code must be numeric"},
    length: { is: 5, :message => "The ZIP Code should be 5 characters"}
